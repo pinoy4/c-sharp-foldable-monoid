e2ab0862bdaccc75d69adda1e90379a43d03a64c
Francesco De Marchi <f.demarchi@mvlabs.it>
2019-11-27 09:55:19 +0100

    fixed problem with default for classes

--- Program.cs
+++ Program.cs
@@ -4,42 +4,42 @@ using System.Collections.Generic;
 
 public class Program
 {
-    public static void Main()
-    {
-        var intMonoids = new List<IntegerMonoid>
-        {
-            new IntegerMonoid(0),
-            new IntegerMonoid(1),
-            new IntegerMonoid(1),
-            new IntegerMonoid(2),
-            new IntegerMonoid(3),
-            new IntegerMonoid(5)
-        };
+	public static void Main()
+	{
+		var intMonoids = new List<IntegerMonoid>
+		{
+			new IntegerMonoid(0),
+			new IntegerMonoid(1),
+			new IntegerMonoid(1),
+			new IntegerMonoid(2),
+			new IntegerMonoid(3),
+			new IntegerMonoid(5)
+		};
 
-        Console.WriteLine($"Fold: {FoldableMonoid.Fold(intMonoids).Value}");
-    }
+		Console.WriteLine($"Fold: {FoldableMonoid.Fold(intMonoids)?.Value}");
+	}
 }
 
-public struct IntegerMonoid: Monoid<IntegerMonoid>
+public class IntegerMonoid : Monoid<IntegerMonoid>
 {
-    public int Value { get; }
+	public int Value { get; }
 
-    public IntegerMonoid(int value)
-    {
-        Value = value;
-    }
+	public IntegerMonoid(int value)
+	{
+		Value = value;
+	}
 
-    public IntegerMonoid AssociativeBinaryOperation(IntegerMonoid x)
-        => new IntegerMonoid(Value + x.Value);
+	public IntegerMonoid AssociativeBinaryOperation(IntegerMonoid x)
+		=> new IntegerMonoid(Value + x?.Value ?? 0);
 }
 
 public interface Monoid<T>
 {
-    T AssociativeBinaryOperation(T x);
+	T AssociativeBinaryOperation(T x);
 }
 
 public static class FoldableMonoid
 {
-    public static T Fold<T>(IEnumerable<T> t) where T : Monoid<T>
-        => t.Aggregate(default(T), (x, y) => x.AssociativeBinaryOperation(y));
+	public static T Fold<T>(IEnumerable<T> t) where T : Monoid<T>
+		=> t.Aggregate(default(T), (x, y) => y.AssociativeBinaryOperation(x));
 }
